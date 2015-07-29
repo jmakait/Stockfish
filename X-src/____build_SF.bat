fd13e14ed1f68762eb8e6afb13f1ae3da59d833e
jmakait <jens.makait@gmx.de>
2015-07-29 21:09:18 +0200

    Revert "Bonus in qsearch if 2nd best move only is a bit worse than the best one"
    This reverts commit ad6f12fb6016bc1d946e2e6df9f9c10362196c58.

--- X-src/____build_SF.bat
+++
@@ -1,8 +0,0 @@
-set PATH=%PATH%;C:\MinGW\mingw64\bin
-echo %PATH%
-
-
-mingw32-make profile-build ARCH=x86-32 COMP=mingw
-mingw32-make profile-build ARCH=x86-64 COMP=mingw
-mingw32-make profile-build ARCH=x86-64-modern COMP=mingw
-pause
